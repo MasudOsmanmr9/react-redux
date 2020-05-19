a3ebef5549a80dc1f381c4f369498214599a94c9
masudosman <masudosmanmr9@gmail.com>
2020-05-19 11:19:11 +0600

    outputted list by props in nested ninja component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React,{Component} from 'react';
 import Ninjas from './Ninjas'
 
 class App extends Component {
+  state = {
+    ninjas:[
+      {name:'masud', age: '26', department:'cse',id:'1'},
+      {name:'rayhan', age: '25', department:'bba',id:'2'},
+      {name:'miraj', age: '26', department:'eee',id:'3'},
+      {name:'rony', age: '23', department:'mit',id:'4'}
+    ]
+  }
   render(){
     return (
       <div className="App">
         <h1>i love you</h1>
-        <Ninjas/>
+        <Ninjas ninjas={this.state.ninjas} />
+        
       </div>
     );
   }
--- src/Ninjas.js
+++ src/Ninjas.js
@@ -2,13 +2,35 @@ import React,{Component} from 'react';
 
 class Ninjas extends Component{
     render(){
+        const {ninjas} = this.props
+        const ninjaList = ninjas.map(ninja=>{
+            const{name,age,department,id} = ninja
+            return(
+                <div key={id}>
+                    <h1>Hy this is {name} Component</h1>
+                    <h3>Welcome :)</h3>
+                    <h3>Name : {name}</h3>
+                    <h3>Age :{age}</h3>
+                    <h3>Department :{department}</h3>
+                </div>
+            );
+        })
         return(
             <div>
-                <h1>Hy this is ninja Component</h1>
-                <h3>Welcome :)</h3>
-                <h3>name : masud</h3>
-                <h3>Welcome :)</h3>
-                <h3>Welcome :)</h3>
+                {/* {ninjas && ninjas.map(ninja=>{
+                    const{name,age,department,id} = ninja
+                    return(
+                        <div key={id}>
+                            <h1>Hy this is {name} Component</h1>
+                            <h3>Welcome :)</h3>
+                            <h3>Name : {name}</h3>
+                            <h3>Age :{age}</h3>
+                            <h3>Department :{department}</h3>
+                        </div>
+                    )
+                })} */}
+                {ninjaList}
+              
             </div>
         )
     }
